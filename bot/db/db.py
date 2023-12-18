a4db3ff7ebb6f4bfce6b8e76f339d30f38187dbc
krolik1591 <dasenkosasa19@gmail.com>
2023-12-18 21:42:36 +0200

    DONE

--- bot/db/db.py
+++ bot/db/db.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from bot.utils.config_reader import config
 
-mongodb = AsyncIOMotorClient(config.mongo_connection_string)["PokemonBotTesting"]
+mongodb = AsyncIOMotorClient(config.mongo_connection_string)["PokemonBot"]
 
 
 if __name__ == '__main__':
